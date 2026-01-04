adf6803bb600553edb32d9c6c03dce3c08e88899
Joanne Basecki <jbasecki@users.noreply.github.com>
2026-01-04 03:50:53 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,18 @@
+import './globals.css'
 
+export const metadata = {
+  title: 'Harmonica Sanctuary',
+  description: 'A Sanctuary for Stashed Cognition',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
